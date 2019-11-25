e9dee5bf0507b3e5772adbd41c6cd0edb8a308cb
Adam Ayd <adam.ayd@gmail.com>
2019-11-25 00:06:15 -0500

    cli_lab completed

---
+++ Intro_To_Python_Development/cli_lab/hr/src/hr/cli.py
@@ -0,0 +1,24 @@
+import argparse
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', help='the path to the export file')
+    parser.add_argument('--format', default='json', choices=['json', 'csv'], type=str.lower)
+    return parser
+
+def main():
+    import sys
+    from hr import export, users
+
+    args = create_parser().parse_args()
+    userslist = users.get_users()
+
+    if args.path:
+        file = open(args.path, 'w', newline='')
+    else:
+        file = sys.stdout
+
+    if args.format == 'json':
+        export.to_json(file, userslist)
+    else:
+        export.to_csv(file, userslist)
